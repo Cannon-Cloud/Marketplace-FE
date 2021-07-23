0cac579aec33f42f771eadafa86e49472124d270
Cannon-Cloud <clarence@cannon.cloud>
2021-07-23 06:49:08 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
