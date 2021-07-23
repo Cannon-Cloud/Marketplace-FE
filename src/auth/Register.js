69a159d832edbcbd057494184a7dfe3076955d4b
Cannon-Cloud <clarence@cannon.cloud>
2021-07-23 11:47:13 -0700

    Added Registration Component

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,6 +1,27 @@
+import React from "react";
+import RegisterForm from "../components/RegisterForm";
+
 const Register = () => {
+  const onSaveRegistrationDataHandler = (enteredUserData) => {
+    const userData = enteredUserData;
+    console.table(userData);
+  };
   return (
-    <div className="container-fluid h1 p-5 text-center">Register Page</div>
+    <React.Fragment>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <RegisterForm
+              onSaveRegistrationData={onSaveRegistrationDataHandler}
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
