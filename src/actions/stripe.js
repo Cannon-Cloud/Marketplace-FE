8efabf90371278cacc7179044053c00fffaca030
Cannon-Cloud <clarence@cannon.cloud>
2021-07-24 12:02:19 -0700

    Started stripe integration

---
+++ src/actions/stripe.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const createConnectAccount = async (token) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/create-connect-account`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
--- src/user/DashboardSeller.js
+++ src/user/DashboardSeller.js
@@ -1,18 +1,29 @@
+import React, { useState } from "react";
 import DashbaordNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { HomeOutlined } from "@ant-design/icons";
+import { createConnectAccount } from "../actions/stripe";
+import { toast } from "react-toastify";
 
 const DashboardSeller = () => {
-  return (
-    <>
-      <div className="constainer-fluid bg-secondary p-5">
-        <ConnectNav />
-      </div>
+  const [isLoading, setIsLoading] = useState(false);
+  const { auth } = useSelector((state) => ({ ...state }));
 
-      <div className="container-fluid p4">
-        <DashbaordNav />
-      </div>
-
+  const clickHandler = async () => {
+    setIsLoading(true);
+    try {
+      let res = await createConnectAccount(auth.token);
+      console.log(res); // get loginlink
+    } catch (err) {
+      console.log(err);
+      toast.error("Stripe connect failed. Try again.");
+      setIsLoading(false);
+    }
+  };
+  const connected = () => {
+    return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-10">
@@ -25,7 +36,53 @@ const DashboardSeller = () => {
           </div>
         </div>
       </div>
-    </>
+    );
+  };
+  const notConnected = () => {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 text-center">
+            <HomeOutlined className="h1" />
+            <h4>Setup payouts to post listings</h4>
+            <p>
+              MERN partners with stripe to transfer earnings to your bank
+              account
+            </p>
+            <button
+              disabled={isLoading}
+              onClick={clickHandler}
+              className="btn btn-primary mb-3"
+            >
+              {isLoading ? "Processing... " : "Setup Payouts"}
+            </button>
+            <p className="text-muted">
+              <small>
+                You will be redirected to stripe to complete the onboarding
+                process
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <div className="constainer-fluid bg-secondary p-5">
+        <ConnectNav />
+      </div>
+      <div className="container-fluid p4">
+        <DashbaordNav />
+      </div>
+      {auth &&
+      auth.user &&
+      auth.user.strip_seller &&
+      auth.user.strip_seller.charges_enabled
+        ? connected()
+        : notConnected()}
+    </React.Fragment>
   );
 };
 
