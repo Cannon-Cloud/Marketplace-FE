4f17541746f396955ab85d5edbfdb57041bc07ac
Cannon-Cloud <clarence@cannon.cloud>
2021-07-26 14:39:13 -0700

    Creat new listings complete

--- src/hotels/NewHotel.js
+++ src/hotels/NewHotel.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 
-import { DatePicker, Select } from "antd";
-import moment from "moment";
-import ReactGoogleAutocomplete from "react-google-autocomplete";
-
-const config = process.env.REACT_APP_GOOGLEPLACES_API_KEY;
-
-const { Option } = Select;
+import { createHotel } from "../actions/hotel";
+import { useSelector } from "react-redux";
+import HotelCreateForm from "../components/Forms/HotelCreateForm";
 
 const NewHotel = () => {
+  //redux
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { token } = auth;
+
   // state
   const [values, setValues] = useState({
     title: "",
@@ -27,10 +27,34 @@ const NewHotel = () => {
   const { title, content, image, price, from, to, bed } = values;
   const [location, setLocation] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(values);
-    console.log(location);
+    // console.log(values);
+    // console.log(location);
+
+    let hotelData = new FormData();
+    hotelData.append("title", title);
+    hotelData.append("content", content);
+    hotelData.append("location", location);
+    hotelData.append("price", price);
+    image && hotelData.append("image", image);
+    hotelData.append("from", from);
+    hotelData.append("to", to);
+    hotelData.append("bed", bed);
+
+    console.log([...hotelData]);
+
+    try {
+      let res = await createHotel(token, hotelData);
+      console.log("Hotel Create Res", res);
+      toast.success("New Hotel Posted");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.response.data);
+    }
   };
 
   const handleImageChange = (e) => {
@@ -43,104 +67,6 @@ const NewHotel = () => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const hotelForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label className="btn btn-outline-secondary btn-block m-2 text-left">
-          Image
-          <input
-            type="file"
-            name="image"
-            onChange={handleImageChange}
-            accept="image/*"
-            hidden
-          />
-        </label>
-
-        <input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          placeholder="Title"
-          className="form-control m-2"
-          value={title}
-        />
-
-        <textarea
-          name="content"
-          onChange={handleChange}
-          placeholder="Content"
-          className="form-control m-2"
-          value={content}
-        />
-
-        <ReactGoogleAutocomplete
-          className="form-control m-2"
-          placeholder="Location"
-          apiKey={config}
-          onPlaceSelected={(place) => {
-            setLocation(place.formatted_address);
-          }}
-          style={{ height: "50px" }}
-        />
-
-        <input
-          type="number"
-          name="price"
-          onChange={handleChange}
-          placeholder="Price"
-          className="form-control m-2"
-          value={price}
-        />
-
-        {/* <input
-          type="number"
-          name="bed"
-          onChange={handleChange}
-          placeholder="Number of Beds"
-          className="form-control m-2"
-          value={bed}
-        /> */}
-
-        <Select
-          onChange={(value) => setValues({ ...values, bed: value })}
-          className="w-100 m-2"
-          size="large"
-          placeholder="Number of beds"
-        >
-          <Option key={1}>{1}</Option>
-          <Option key={2}>{2}</Option>
-          <Option key={3}>{3}</Option>
-          <Option key={4}>{4}</Option>
-        </Select>
-      </div>
-
-      <DatePicker
-        placeholder="From date"
-        className="form-control m-2"
-        onChange={(date, dateString) =>
-          setValues({ ...values, from: dateString })
-        }
-        disabledDate={(current) =>
-          current && current.valueOf() < moment().subtract(1, "days")
-        }
-      />
-
-      <DatePicker
-        placeholder="To date"
-        className="form-control m-2"
-        onChange={(date, dateString) =>
-          setValues({ ...values, to: dateString })
-        }
-        disabledDate={(current) =>
-          current && current.valueOf() < moment().subtract(1, "days")
-        }
-      />
-
-      <button className="btn btn-outline-primary m-2">Save</button>
-    </form>
-  );
-
   return (
     <>
       <div className="container-fluid bg-secondary p-5 text-center">
@@ -150,7 +76,15 @@ const NewHotel = () => {
         <div className="row">
           <div className="col-md-10">
             <br />
-            {hotelForm()}
+            <HotelCreateForm
+              values={values}
+              setValues={setValues}
+              handleChange={handleChange}
+              handleImageChange={handleImageChange}
+              handleSubmit={handleSubmit}
+              location={location}
+              setLocation={setLocation}
+            />
           </div>
           <div className="col-md-2">
             <img
